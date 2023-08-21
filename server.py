e279044ea94a0c3c47e291ff5fc0512f804738cb
Yura <gyrra777@gmail.com>
2023-08-21 21:34:57 +0300

    работа над ошибками

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import http.client
+import json
 
 
 def handle_request(client_socket):
@@ -23,22 +24,45 @@ def handle_request(client_socket):
     if '/?status=' in request:
         status_code = request.split('/?status=')[1].split()[0]
         try:
-            status = http.client.responses[int(status_code)]
-        except KeyError:
-            # Если указан невалидный статус, возвращаем ошибку 400 Bad Request
-            response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
+            if status_code.isdigit():
+                status = http.client.responses[int(status_code)]
+            elif status_code.isalpha():
+                status = http.client.responses[str(status_code)]
+        except BaseException:
+            status_code = '200'
+            json_body = collecting_json(headers, method, status, status_code)
+            response = collecting_response(json_body, status, status_code)
             client_socket.send(response.encode())
             client_socket.close()
             return
 
-    #  ответ сервера
-    response = f'HTTP/1.1 {status_code} {status}\r\n'
-    for header in headers:
-        response += header + '\r\n'
-    response += '\r\n'
+    json_body = collecting_json(headers, method, status, status_code)
+
+    response = collecting_response(json_body, status, status_code)
 
     # Отправляем ответ
     client_socket.send(response.encode())
+
+
+def collecting_response(json_body, status, status_code):
+    # Формируем ответ сервера
+    response = f'HTTP/1.1 {status_code} {status}\r\n'
+    response += 'Content-Type: application/json\r\n'
+    response += f'Content-Length: {len(json_body)}\r\n'
+    response += '\r\n'
+    response += json_body
+    return response
+
+
+def collecting_json(headers, method, status, status_code):
+    # Формируем JSON body
+    json_body = {
+        'headers': headers,
+        'method': method,
+        'status': status_code + ' ' + status
+    }
+    json_body = json.dumps(json_body)
+    return json_body
 
 
 def run_server():
@@ -54,8 +78,9 @@ def run_server():
 
     print('Server is running....')
 
-    client_socket, client_address = server_socket.accept()
-    handle_request(client_socket)
+    while True:
+        client_socket, client_address = server_socket.accept()
+        handle_request(client_socket)
 
 
 run_server()
